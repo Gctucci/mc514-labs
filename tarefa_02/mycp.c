52d835cbcccb09a570952100c00d85166436889d
gabrielnatucci@gmail.com <gabrielnatucci@gmail.com@f7d3909d-d2c0-1276-5ca8-a86fbd1537df>
2011-04-28 04:19:29 +0000

    Criada a função mycp3.c - Acho que tá certinha.
    Falta criar a mycp4.c ...

--- tarefa_02/mycp.c
+++ tarefa_02/mycp.c
@@ -5,42 +5,43 @@
  * Função main
  * @param argc numero de argumento passados na entrada
  * @param argv vetor com os valores dos argumentos passados
- * 
+ *
  * @returns 1 se houver algum erro, 0 caso contrário
  */
 int main(int argc, char** argv){
 
-	
+       
 
-	if(argc < 3){
-		printf("Error while reading the arguments. They may be invalid or inexistent.\n");
-		printf("Right mode: ./mycp [archive] [destination] [type_of_copy -optional]\n");
-		exit(-1);
-	}
-	else if(argc == 3){
-		mycp1(argv);
-	}
-	else {
-		if(!strcmp(argv[3],"mycp1")){
-			mycp1(argv);
-		}
-		else if(!strcmp(argv[3],"mycp2")){
-			mycp2(argv);
-		}/*
-		else if(argv[3] == "mycp3"){
-			mycp3(argv);
-		}
-		else if(argv[3] == "mycp4"){
-			mycp4(argv);
-		}*/
-		else{
-			printf("ERROR: parameter not recognized;\n");
-			exit(-1);
-		}
-	}
+        if(argc < 3){
+                printf("Error while reading the arguments. They may be invalid or inexistent.\n");
+                printf("Right mode: ./mycp [archive] [destination] [type_of_copy -optional]\n");
+                exit(-1);
+        }
+        else if(argc == 3){
+                mycp1(argv);
+        }
+        else {
+                if(!strcmp(argv[3],"mycp1")){
+                        mycp1(argv);
+                }
+                else if(!strcmp(argv[3],"mycp2")){
+                        mycp2(argv);
+                }
+                else if(!strcmp(argv[3],"mycp3")){
+                        mycp3(argv);
+                }/*
+                else if(!strcmp(argv[3],"mycp4")){
+                        mycp4(argv);
+                }*/
+                else{
+                        printf("ERROR: parameter not recognized;\n");
+                        exit(-1);
+                }
+        }
 
 
 
 return 0;
 }
+
 
