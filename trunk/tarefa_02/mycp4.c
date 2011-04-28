027bc324ae70e22a3a28d653f7d1fb2bce3b10a2
gabrielnatucci@gmail.com <gabrielnatucci@gmail.com@f7d3909d-d2c0-1276-5ca8-a86fbd1537df>
2011-04-28 21:20:14 +0000

    funçao mmap - precisa ser melhorada

--- trunk/tarefa_02/mycp4.c
+++ trunk/tarefa_02/mycp4.c
@@ -1,84 +1,68 @@
 #include "mycp.h"
+#include <sys/mman.h>
 
 /**
- * Função que copia um arquivo utilizando as funções da aio.h
+ * Função que copia um arquivo utilizando as funções da mman.h
  * 
  * @param files Nomes dos arquivos de origem e destino
  */
 int mycp4(char** files){
 
-	int indescr, outdescr, reader, writer;
+	
+	void *src, *dst;
+	struct stat statbuf;
+	int indescr, outdescr;
 	char buffer[MAXBUFF];
-	struct aiocb data_in;
-	struct aiocb data_out;
 
 	if(files == NULL){
 		printf("Error while opening files: check if there any arguments were passed.\n");
 		exit(-1);
 	}
 
-	// Abre o arquivo de entrada
 	indescr = open(files[1], O_RDONLY);
+
+
 	if (indescr < 0){ 
 		printf("ERROR: File cannot be opened.\n");
 		exit(-1);
 	}
-	
-	// Abre o arquivo de saída
+
 	chdir(files[2]);
-	outdescr = creat(files[1], OUTPUT);
+	outdescr = open(files[1], O_RDWR | O_CREAT | O_TRUNC,FILE_MODE);
+
 	if (outdescr < 0){ 
 		printf("ERROR: File cannot be created.\n");
 		exit(-1);
 	}
-	
-	// Struct de dados do arquivo de entrada
-	memset(&data_in, 0, sizeof(struct aiocb));
-	data_in.aio_nbytes = MAXBUFF;
-	data_in.aio_fildes = indescr;
-	data_in.aio_offset = 0;
-	data_in.aio_buf = buffer;
-
-	// Struct de dados do arquivo de saida
-	memset(&data_out, 0, sizeof(struct aiocb));
-	data_out.aio_nbytes = MAXBUFF;
-	data_out.aio_fildes = outdescr;
-	data_out.aio_offset = 0;
-	data_out.aio_buf = buffer;
-
-
-	// Loop que copia o arquivo
-	while (TRUE) {
-		reader = aio_read(&data_in); // le um bloco de dados
-		
-		// Se ocorrer um erro durante a cópia, ou o arquivo acabar, o loop pára.
-		if (reader == -1) break;
-		
-		// Espera até acabar de ler
-		//while(aio_error(&data_in) == EINPROGRESS);
-		
-		writer = aio_write(&data_out); // escreve um bloco de dados
-		
-		// Se ocorrer um erro durante a cópia, ou o arquivo acabar, o loop pára.
-		if (writer == -1){
+	if (fstat(indescr, &statbuf) < 0){ /* need size of input file */
+		printf("ERROR: Could not determine the size of file.\n");
+		exit(-1);
+	}
+	while(TRUE){
+		/* set size of output file */
+		if (lseek(outdescr, statbuf.st_size - 1, SEEK_SET) == -1)
+			break;
+		if (write(outdescr,buffer, 1) != 1){
 			printf("Error while writing file: File could not be written.\n");
 			exit(-1);
 		}
-		
-		// Espera até acabar de escrever
-		//while(aio_error(&data_out) == EINPROGRESS);
+		src = mmap(0, statbuf.st_size, PROT_READ, MAP_SHARED,indescr, 0);
+		if (src == MAP_FAILED){
+			printf("ERROR: mmap failed for input.\n");	
+			exit(-1);
+		}
+		dst = mmap(0, statbuf.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, outdescr, 0);
+		if (dst == MAP_FAILED){
+			printf("ERROR: mmap failed for output.\n");	
+			exit(-1);
+		}
+		memcpy(dst, src, statbuf.st_size); /* does the file copy */
 	}
 	/*Fechando os arquivos/diretórios*/
-	close(indescr); close(outdescr);
-
-	if (reader == 0){
-		printf("File successfully copied!\n"); 
-		exit(0);
-	} 
-	else{
-		printf("Error while writing file. Please try again.\n");
-		exit(-1);
-	}
+	close(indescr); 
+	close(outdescr);
+	
+	printf("File successfully copied!\n"); 
 
 	return 0;
 }
