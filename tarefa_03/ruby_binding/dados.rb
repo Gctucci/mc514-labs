357c94d9f6eef79bb78178719c504d334ff50712
fernandolbastos@gmail.com <fernandolbastos@gmail.com@f7d3909d-d2c0-1276-5ca8-a86fbd1537df>
2011-06-02 03:48:47 +0000

    script para juntar tudo e fazer os histogramas

--- tarefa_03/ruby_binding/dados.rb
+++ tarefa_03/ruby_binding/dados.rb
@@ -1,3 +1,39 @@
 require 'ffi'
-require 'ext2fs'
+#require 'ext2fs'
 
+if ARGV.count < 2
+	puts "Uso: ruby dados.rb [opção] [arquivo]"
+	puts "\topção: Partição ou arquivo ext a ser examinado"
+	puts "\t\t1: Fragmentação de blocos alocados em no. de segmentos"
+	puts "\t\t2: Tamanhos de arquivos em blocos de alocação"
+	puts "\tarquivo: Partição ou arquivo ext a ser examinado"
+else
+	if ARGV[0].to_i == 1
+		puts "Histograma de fragmentação de blocos alocados em no. de segmentos"
+	elsif ARGV[0].to_i == 2
+		puts "Histograma de tamanhos de arquivos em blocos de alocação"
+	else
+		puts "Opção inválida"
+	end
+	puts 10
+	puts 10
+	puts 10
+	puts 10
+	puts 10
+	puts 10
+	puts 10
+	puts 10
+	puts 10
+	puts 10
+	puts 10
+	puts 10
+	puts 10
+	puts 10
+	puts 10
+	puts 10
+	puts 10
+	puts 10
+	puts 10
+	puts 10
+	puts 10
+end
