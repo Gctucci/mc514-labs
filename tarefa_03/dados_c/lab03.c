f24716aa9e29e821f94276c5628d4ec7d77f6eb5
fernandolbastos@gmail.com <fernandolbastos@gmail.com@f7d3909d-d2c0-1276-5ca8-a86fbd1537df>
2011-06-02 19:55:53 +0000

    mudando alguns ponteiros

--- tarefa_03/dados_c/lab03.c
+++ tarefa_03/dados_c/lab03.c
@@ -8,22 +8,31 @@
 
 int main(int argc, char **argv){
 
-	char * name;
+	const char * name;
 	ext2_filsys  fs;
 	errcode_t error;
-	ext2_inode_scan *scan;
+	ext2_inode_scan scan;
+	struct ext2_inode *inode;
+	ext2_ino_t *inode_number;
+	int i;
+	
 
 	if(argc < 2){
 		printf("Numero de argumentos inválidos. Favor digitar o sistema de arquivos.\n");
 	}
 	else{
 		strcpy(argv[1],name);
-		error = ext2fs_open(name,0,0,0,unix_io,&fs);
+		error = ext2fs_open(name,0,0,0,unix_io_manager,&fs);
 		if(error){
 			printf("Houve um problema ao abrir o filesystem.\n");
 		}
-		ext2fs_open_inode_scan (fs,0,scan); //Prepara a variavel scan para iterar sobre os inodes
-		
+		ext2fs_open_inode_scan (fs,0,&scan); //Prepara a variavel scan para iterar sobre os inodes
+
+		for(i=0;i<scan.s_inodes_count;i++){
+			ext2fs_get_next_inode (scan,inode_number,inode);
+			
+		}
+		ext2fs_close_inode_scan(scan);
 		error = ext2fs_close (fs);
 		if(error){
 			printf("Houve um problema ao fechar o filesystem.\n");
