e839a9ead8ccbfb3f9cd8e9313f2b4684fb0300a
fernandolbastos@gmail.com <fernandolbastos@gmail.com@f7d3909d-d2c0-1276-5ca8-a86fbd1537df>
2011-06-03 02:47:10 +0000

    mudanças no meu laptop

--- trunk/tarefa_03/histograma/script.js
+++ trunk/tarefa_03/histograma/script.js
@@ -4,6 +4,7 @@ function makeGraph()
 	var labels = document.getElementById("labels");
 	var dnl = container.getElementsByTagName("li");
 	var lastyear = 0;
+	var max = 0;
 	
 	for(var i = 0; i < dnl.length; i++)
 	{
@@ -12,10 +13,24 @@ function makeGraph()
 		var content = value.split(":");
 		var color =item.style.background;
 		value = content[0];
-		item.style.height= value + "px";
-		item.style.top=(199 - value) + "px";
+		if(parseInt(max) < parseInt(value))
+		{
+			max = value;
+		}
+	}
+	
+	for(var i = 0; i < dnl.length; i++)
+	{
+		var item = dnl.item(i);
+		var value = item.innerHTML;
+		var content = value.split(":");
+		var color =item.style.background;
+		value = content[0];
+		value_rel = 200*value/max
+		item.style.height= value_rel + "px";
+		item.style.top=(199 - value_rel) + "px";
 		item.style.left = (i * 50 + 20) + "px";
-		item.style.height = value + "px";
+		item.style.height = value_rel + "px";
 		item.innerHTML = value;
 		item.style.visibility="visible";
 		if(content.length > 2)
@@ -27,7 +42,13 @@ function makeGraph()
 		
 		labels.innerHTML = labels.innerHTML + "<span style='position:absolute;top:-16px;left:"+ left+";background:"+ color+"'>" + content[1] + "</span>";
 		
-	}	
+	}
+	
+	for (var i=3; i>=0; i--)
+	{
+		document.getElementById("scale").innerHTML += new String(parseInt(i*max/3));
+		document.getElementById("scale").innerHTML += "<br \\><br \\><br \\>";
+	}
 }
 
 window.onload=makeGraph;
