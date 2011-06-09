f680273e92de9f98ea62e672f41e37268d8d01ed
fernandolbastos@gmail.com <fernandolbastos@gmail.com@f7d3909d-d2c0-1276-5ca8-a86fbd1537df>
2011-06-09 10:47:20 +0000

    Renomeado script do ruby(não funciona); Finalizado script do C; Corrigido script do histograma(sed para titulos); Corrigida posição da opção 3 no código C

--- trunk/tarefa_03/histograma/histograma.sh
+++ trunk/tarefa_03/histograma/histograma.sh
@@ -6,9 +6,9 @@ read titulo
 # Parte inicial do html, com o título substituído
 if [ -e ./partes/top.html ]
 then
-	cat ./partes/top.html
+	cat ./partes/top.html | sed "s/titulo/${titulo}/"
 else
-	cat ./histograma/partes/top.html
+	cat ./histograma/partes/top.html | sed "s/titulo/${titulo}/"
 fi
 
 # Lê os dados e conta cada categoria
--- trunk/tarefa_03/obter_dados_c.sh
+++ trunk/tarefa_03/obter_dados_c.sh
@@ -4,14 +4,23 @@ then
 	echo "Uso: ./obter_dados.sh [caminho absoluto para o arquivo/partição]"
 else
 	hist_1=fragmentação.html
+	hist_1_titulo="Histograma de fragmentação de blocos alocados em no. de segmentos"
 	hist_2=tamanhos.html
+	hist_2_titulo="Histograma de tamanhos de arquivos em blocos de alocação"
 	
 	# Gera o primeiro histograma
 	echo "Gerando o histograma de fragmentação de blocos alocados em no. de segmentos"
-	(cd ./dados_c && ./dados 1 $1 ) | (cd ./histograma && ./histograma.sh) > $hist_1
+	(echo $hist_1_titulo && cd ./dados_c && ./dados 1 $1 ) | (cd ./histograma && ./histograma.sh) > $hist_1
+	echo "Arquivo $hist_1 gerado"
 	
 	# Gera o segundo histograma
 	echo "Gerando o histograma de tamanhos de arquivos em blocos de alocação"
-	(cd ./dados_c && ./dados 2 $1 ) | (cd ./histograma && ./histograma.sh) > $hist_2
+	(echo $hist_2_titulo && cd ./dados_c && ./dados 2 $1 ) | (cd ./histograma && ./histograma.sh) > $hist_2
+	echo "Arquivo $hist_2 gerado"
+	echo
+	
+	# Obtém os dados pedidos
+	echo "Obtendo os dados do sistema de arquivos:"
+	cd ./dados_c && ./dados 3 $1
 	echo
 fi
